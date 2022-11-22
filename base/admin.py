3795660cff447a18e545e6d846286283f6cf0ffe
Just Mubashir <codeatemail@gmail.com>
2022-11-23 01:52:12 +0530

    admin added for profile changes

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import  City,State,Country,Category,Post
+from . models import  City,State,Country,Category,Post,Profile
 
 admin.site.register(City)
 admin.site.register(State)
 admin.site.register(Country)
 admin.site.register(Category)
 admin.site.register(Post)
+admin.site.register(Profile)
