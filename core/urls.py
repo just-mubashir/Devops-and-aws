099c4946ae1c8ad2995228d3ac80e92a07a48da5
Just Mubashir <codeatemail@gmail.com>
2022-11-23 01:06:06 +0530

    static changes

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,11 @@ urlpatterns = [
     path('', include("django.contrib.auth.urls"))
 ]
 
+
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
-        urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
